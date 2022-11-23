fcfc0abb48c5ea4c327bb39558a91f097b99779b
rydmr <115474621+rydmr@users.noreply.github.com>
2022-11-23 14:39:49 +0400

    dhjdhf

---
+++ src/assets/components/Product.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Price from "./Price"
+import ProdectName from "./ProductName"
+
+
+const Product = () => {
+
+  return (
+    <>
+    <ProdectName/>
+    <Price/>
+    </>
+  )
+}
+
+export default Product
---
+++ src/assets/components/ProductName.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProductName = () => {
+return (
+    <div>
+        <h3>Roya Demirova</h3>
+    </div>
+  )
+}
+
+export default ProductName
